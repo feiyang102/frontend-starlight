8c3178a248fa2bdc37a7a2174c01ef1142cf9bd7
feiyang <feiyang102@outlook.com>
2023-05-04 11:40:11 +0800

    疑问:leetcode912:排序数组性能问题

---
+++ 算法/英雄哥/9日训练营/Day4/重新排列数组.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int test(int *nums[])
+{
+    printf("%d", nums[0]);
+    printf("asdfji");
+}
+
+int main()
+{
+    int c = 12300 % 1000;
+    int a[11] = {5, 2, 0};
+    test(a);
+    printf("asfsdfasf");
+}
