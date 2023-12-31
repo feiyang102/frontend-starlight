32881406a5594f250a907706ec87d83002836cd1
feiyang <feiyang102@outlook.com>
2024-01-01 01:49:54 +0800

    admin-pro初始化项目，创建基础文件

---
+++ Practice/Vue/admin-pro/src/main.ts
@@ -0,0 +1,6 @@
+// TODO: import { createApp } from "vue@3.4.3";
+import { createApp } from "vue";
+import App from './App.vue'
+import './styles/index.css'
+const app = createApp(App);
+app.mount("app");
