a2e606bc5eaa3c9a559ced18f747a1bc62cc1891
feiyang <feiyang102@outlook.com>
2024-01-05 00:49:20 +0800

    admin-pro:封装Pinia Store

--- Practice/Vue/admin-pro/src/views/login/index.vue
+++ Practice/Vue/admin-pro/src/views/login/index.vue
@@ -4,6 +4,9 @@
 
 <script lang="ts" setup>
 // import { reactive, toRefs, ref, onBeforeMount, onMounted } from "vue";
+import { useUserStore } from '@/store/user';
+
+const userStore = useUserStore();
 </script>
 
 <style scoped></style>
