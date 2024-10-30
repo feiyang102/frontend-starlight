0ce45a38d19119903a1c436aa1d4615aec3adc66
feiyang102 <feiyang102@outlook.com>
2024-10-30 23:20:28 +0800

    demo: vue3-use-ts

---
+++ demos/vue3-use-ts/src/components/TsComp1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>你好{{ test }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const test = ref<string>('123123asfsd');
+test.value = 'Hello TypeScript111'
+console.log(test?.value?.hello)
+</script>
+
+<style scoped></style>
